9aa8d9b22cbf8d5638f337ae70d4b11d77add29f
Décio Oliveira Jr <decio.oliveira@usp.br>
2017-09-01 20:35:40 -0300

    Final Aula 1 - Usando Threads

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -36,12 +36,10 @@ namespace ByteBank.View
         {
             var contas = r_Repositorio.GetContaClientes();
 
-            var contasQuantidadePorThread = contas.Count() / 4;
+            var contasQuantidadePorThread = contas.Count() / 2;
 
             var contas_parte1 = contas.Take(contasQuantidadePorThread);
-            var contas_parte2 = contas.Skip(contasQuantidadePorThread).Take(contasQuantidadePorThread);
-            var contas_parte3 = contas.Skip(contasQuantidadePorThread*2).Take(contasQuantidadePorThread);
-            var contas_parte4 = contas.Skip(contasQuantidadePorThread*3);
+            var contas_parte2 = contas.Skip(contasQuantidadePorThread);
 
             var resultado = new List<string>();
 
@@ -65,30 +63,12 @@ namespace ByteBank.View
                     resultado.Add(resultadoProcessamento);
                 }
             });
-            Thread thread_parte3 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte3)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
-            Thread thread_parte4 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte4)
-                {
-                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoProcessamento);
-                }
-            });
+           
 
             thread_parte1.Start();
             thread_parte2.Start();
-            thread_parte3.Start();
-            thread_parte4.Start();
 
-            while (thread_parte1.IsAlive || thread_parte2.IsAlive
-                || thread_parte3.IsAlive || thread_parte4.IsAlive )
+            while (thread_parte1.IsAlive || thread_parte2.IsAlive)
             {
                 Thread.Sleep(250);
                 //Não vou fazer nada
