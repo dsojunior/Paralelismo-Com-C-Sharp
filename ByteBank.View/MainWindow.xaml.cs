c292d4898445ed84b40d703fbf97e4394954a3c4
Décio Oliveira Jr <decio.oliveira@usp.br>
2017-12-20 23:52:35 -0200

    Final Aula 5 - Criando tarefas canceláveis
    Final Aula 5 - Criando tarefas canceláveis

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace ByteBank.View
     {
         private readonly ContaClienteRepository r_Repositorio;
         private readonly ContaClienteService r_Servico;
+        private CancellationTokenSource _cts;
 
         public MainWindow()
         {
@@ -38,6 +39,8 @@ namespace ByteBank.View
             
             BtnProcessar.IsEnabled = false;
 
+            _cts = new CancellationTokenSource();
+
             var contas = r_Repositorio.GetContaClientes();
 
             PgsProgresso.Maximum = contas.Count();
@@ -46,36 +49,59 @@ namespace ByteBank.View
 
             var inicio = DateTime.Now;
 
+            BtnCancelar.IsEnabled = true;
+
             var progress = new Progress<String>(str =>
                 PgsProgresso.Value++);
 
             //var bytebankProgress = new ByteBankProgress<string>(str =>
             //    PgsProgresso.Value++);
 
-            var resultado = await ConsolidarContas(contas, progress);
 
-            var fim = DateTime.Now;
-            AtualizarView(resultado, fim - inicio);
-            BtnProcessar.IsEnabled = true;
-            
+            try
+            {
+
+                var resultado = await ConsolidarContas(contas, progress, _cts.Token);
+                var fim = DateTime.Now;
+                AtualizarView(resultado, fim - inicio);
+            }
+            catch (OperationCanceledException)
+            {
+                TxtTempo.Text = "Operação cancelada pelo usuário";
+            }
+            finally
+            {
+                BtnProcessar.IsEnabled = true;
+                BtnCancelar.IsEnabled = false;
+            }
         }
 
-        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso)
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            BtnCancelar.IsEnabled = false;
+            _cts.Cancel();
+        }
+
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas, IProgress<string> reportadorDeProgresso, CancellationToken ct)
         {
            
             var tasks = contas.Select(conta =>
                 Task.Factory.StartNew(() =>
                 {
-                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta);
+
+                    ct.ThrowIfCancellationRequested();
+                    
+                    var resultadoConsolidacao = r_Servico.ConsolidarMovimentacao(conta, ct);
 
                     // Não utilizaremos atualização do PgsProgresso na Thread de Trabalho
                     // PgsProgresso.Value++;
-
-                    reportadorDeProgresso.Report(resultadoConsolidacao);
                     
+                    reportadorDeProgresso.Report(resultadoConsolidacao);
+
+                    ct.ThrowIfCancellationRequested();
 
                     return resultadoConsolidacao;
-                })
+                }, ct)
              );
 
             return await Task.WhenAll(tasks);
@@ -97,5 +123,7 @@ namespace ByteBank.View
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
         }
+
+        
     }
 }
